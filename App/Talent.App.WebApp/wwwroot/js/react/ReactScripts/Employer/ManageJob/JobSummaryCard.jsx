63656ece8a266691f2dcf68c468931bbc2335712
Carlito Pardales <carlpardales@gmail.com>
2020-01-07 15:05:18 +1300

    Display jobs as a list of cards. Implemented Update and Close job functionalities.

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -6,14 +6,86 @@ import moment from 'moment';
 export class JobSummaryCard extends React.Component {
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
         this.selectJob = this.selectJob.bind(this)
+        this.renderExpiredLabel = this.renderExpiredLabel.bind(this);
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        this.selectJob(this.props.id);
     }
 
     selectJob(id) {
         var cookies = Cookies.get('talentAuthToken');
-        //url: 'http://localhost:51689/listing/listing/closeJob',
+        var link = 'http://localhost:51689/listing/listing/closeJob';
+
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(id),
+            success: function (res) {
+                if (res.success) {
+                    TalentUtil.notification.show(res.message, "success", null, null);
+                }
+                else {
+                    TalentUtil.notification.show(res.message, "error", null, null);
+                }
+            }.bind(this),
+            error: function (res) {
+                TalentUtil.notification.show(res.message, "error", null, null);
+            }.bind(this)
+        })
+    }
+
+    renderExpiredLabel(expiryDate) {
+        // Need to add condition to check if job is expired before rendering the label.
+        return (
+            <div className="ui small red label">Expired</div>
+        );
     }
 
     render() {
+        const editJobPath = `/EditJob/${this.props.id}`;
+        const copyJobPath = `/PostJob/${this.props.id}`;
+
+        return (
+            <div className="ui card">
+                <div className="content">
+                    <div className="header">{this.props.title}</div>
+                    <a className="ui black right ribbon label">
+                        <i aria-hidden="true" className="user icon"></i>
+                        {this.props.noOfSuggestions}
+                    </a>
+                    <div className="meta">{this.props.location}</div>
+                    <div className="description job-summary">
+                        {this.props.summary}
+                    </div>
+                </div>
+                <div className="extra content">
+                    {this.renderExpiredLabel(this.props.expiryDate)}
+                    <div className="ui compact mini right floated buttons">
+                        <button className="ui blue basic button" onClick={this.handleClick}>
+                            <i aria-hidden="true" className="ban icon"></i>
+                            Close
+                        </button>
+                        <a className='ui blue basic button' href={editJobPath}>
+                            <i aria-hidden="true" className="edit icon"></i>
+                            Edit
+                        </a>
+                        <a className='ui blue basic button' href={copyJobPath}>
+                            <i aria-hidden="true" className="copy outline icon"></i>
+                            Copy
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -7,6 +7,20 @@ import { JobSummaryCard } from './JobSummaryCard.jsx';
 import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
 import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
 
+const filterOptions = [
+    { key: '0', value: 'Choose Filter', text: 'Choose Filter' },
+    { key: '1', value: 'Active', text: 'Active' },
+    { key: '2', value: 'Closed', text: 'Closed' },
+    { key: '3', value: 'Draft', text: 'Draft' },
+    { key: '4', value: 'Expired', text: 'Expired' },
+    { key: '5', value: 'Unexpired', text: 'Unexpired' }
+];
+
+const sortByDate = [
+    { key: '0', value: 'Newest first', text: 'Newest first' },
+    { key: '1', value: 'Oldest first', text: 'Oldest first' }
+];
+
 export default class ManageJob extends React.Component {
     constructor(props) {
         super(props);
@@ -18,6 +32,8 @@ export default class ManageJob extends React.Component {
             loadJobs: [],
             loaderData: loader,
             activePage: 1,
+            recordsPerPage: 6,
+            totalPages: 1,
             sortBy: {
                 date: "desc"
             },
@@ -34,30 +50,74 @@ export default class ManageJob extends React.Component {
         this.loadData = this.loadData.bind(this);
         this.init = this.init.bind(this);
         this.loadNewData = this.loadNewData.bind(this);
-        //your functions go here
+        this.handleChange = this.handleChange.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
     };
 
     init() {
         let loaderData = TalentUtil.deepCopy(this.state.loaderData)
         loaderData.isLoading = false;
-        this.setState({ loaderData });//comment this
+        // this.setState({ loaderData });//comment this
 
         //set loaderData.isLoading to false after getting data
-        //this.loadData(() =>
-        //    this.setState({ loaderData })
-        //)
-        
-        //console.log(this.state.loaderData)
+        this.loadData(() =>
+            this.setState({ loaderData })
+        )
     }
 
     componentDidMount() {
         this.init();
     };
 
+    handleChange(event, data) {
+        //Add dropdown selection handling here
+        console.log('Unhandled dropdown');
+    }
+
+    handlePageChange(event, { activePage }) {
+        if (activePage !== this.state.activePage) {
+            this.setState({
+                activePage: activePage
+            }, this.loadData);
+        }
+    }
+
     loadData(callback) {
+        const activePage = this.state.activePage;
+        const sortbyDate = this.state.sortBy.date;
+        const { showActive, showClosed, showDraft, showExpired, showUnexpired } = this.state.filter;
+
+        const params = '?activePage=' + activePage + '&sortbyDate=' + sortbyDate + '&showActive=' + showActive +
+            '&showClosed=' + showClosed + '&showDraft=' + showDraft + '&showExpired=' + showExpired +
+            '&showUnexpired=' + showUnexpired;
+
         var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
         var cookies = Cookies.get('talentAuthToken');
-       // your ajax call and other logic goes here
+
+        $.ajax({
+            url: link + params,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (res) {
+                if (res.myJobs) {
+                    this.setState({
+                        loadJobs: res.myJobs,
+                        totalPages: Math.ceil(res.totalCount / this.state.recordsPerPage)
+                    })
+                }
+                if (callback && typeof (callback) === "function") {
+                    callback();
+                }
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        })
     }
 
     loadNewData(data) {
@@ -75,9 +135,57 @@ export default class ManageJob extends React.Component {
     }
 
     render() {
+        const jobs = this.state.loadJobs;
         return (
             <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               <div className ="ui container">Your table goes here</div>
+                <div className="ui container">
+                    <div className="ui grid">
+                        <h2 className="ui header">List of Jobs</h2>
+                        <div className="row">
+                            <div className="padded column">
+                                <i aria-hidden="true" className="filter icon"></i>
+                                Filter:{' '}
+                                <Dropdown
+                                    inline
+                                    options={filterOptions}
+                                    defaultValue={filterOptions[0].value}
+                                />
+                                <i aria-hidden="true" className="calendar icon"></i>
+                                Sort by date:{' '}
+                                <Dropdown
+                                    inline
+                                    options={sortByDate}
+                                    defaultValue={sortByDate[0].value}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <div className="ui three cards">
+                                    {jobs.map(job =>
+                                        <JobSummaryCard
+                                            key={job.id}
+                                            id={job.id}
+                                            title={job.title}
+                                            noOfSuggestions={job.noOfSuggestions}
+                                            location={`${job.location.city}, ${job.location.country}`}
+                                            summary={job.summary}
+                                            expiryDate={job.expiryDate} />
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="center aligned padded column">
+                                <Pagination
+                                    defaultActivePage={this.state.activePage}
+                                    totalPages={this.state.totalPages}
+                                    onPageChange={this.handlePageChange} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </BodyWrapper>
         )
     }
